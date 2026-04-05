b43360ae54b1291804fbdf5745ae5b1f1bb6fb97
Ahmed-Alanaswah <alanaswehahmed0@gmail.com>
2026-04-05 23:31:10 +0300

    news app

---
+++ app/news/[slug]/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div id="error">
+      <h1>Not Found!</h1>
+      <p>unfortunatly, we could not find the requested article!</p>
+    </div>
+  );
+}
